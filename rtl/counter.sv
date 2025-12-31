8451140ce407b26e6c5ed84b168b74828eb033f5
Carlos Rojas <chihuahuaplate@gmail.com>
2025-12-30 22:33:38 -0800

    changes in styling, seperated next state logic from always_ff and casing of parameters

--- rtl/counter.sv
+++ rtl/counter.sv
@@ -1,54 +1,43 @@
 `timescale 1ns / 1ps
-`default_nettype none
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/18/2025 02:34:59 PM
-// Design Name: 
-// Module Name: counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+// TODO: Include box comments
 
 module counter
-  #(parameter width_p = 8
-   ,parameter [width_p-1:0] reset_val_p = '0
-   )
-   (input wire clk_i
-   ,input wire reset_i
-   ,input wire en_i
-   ,input wire up_i
-   ,input wire dw_i
-   ,output wire [width_p-1:0] count_o
+  #(parameter WIDTH_P = 8
+   ,parameter [WIDTH_P-1:0] RESET_VAL_P = '0
+    )
+  (input clk_i
+  ,input reset_i
+  ,input en_i
+  ,input up_i
+  ,input dw_i
+  ,output [WIDTH_P-1:0] count_o
    );
-   
-   logic [width_p-1:0] count_r;
-   
-   always_ff @(posedge clk_i) begin
-    if (reset_i) begin
-        count_r <= reset_val_p;
-    end
-    else begin
-        if (en_i) begin
-            if (up_i && (up_i ^ dw_i))
-                count_r <= count_r + 1;
-            else if (dw_i && (up_i ^ dw_i))
-                count_r <= count_r - 1;
-        end
-      end
-   end
 
-   assign count_o = count_r;
-   
+  
+  logic [WIDTH_P-1:0] count_r, count_n;
+
+  // Next state logic
+  always_comb begin
+    count_n = count_r;
+
+    case ({up_i, dw_i})
+      2'b01: count_n = count_n - 1;
+      2'b10: count_n = count_n + 1;
+      default: count_n = count_r;
+    endcase
+  end
+  
+  always_ff @(posedge clk_i) begin
+    if (reset_i) begin
+      count_r <= RESET_VAL_P;
+    end
+    else if (en_i) begin
+      count_r <= count_n;
+    end
+  end
+
+  // output assigns
+  assign count_o = count_r;
+  
 endmodule
